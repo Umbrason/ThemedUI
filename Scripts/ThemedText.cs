b1e4232ac017d34c0c0fe0ba64fa6b0146ee8c92
Umbrason <FabioKolligs@outlook.de>
2021-04-22 17:15:32 +0200

    fixed bug in HDRP

--- Scripts/ThemedText.cs
+++ Scripts/ThemedText.cs
@@ -22,10 +22,7 @@ public class ThemedText : Text
             vertex.position = temp.vertices[i];
             vertex.normal = temp.normals[i];
             //constrain uv to .00005f, .99995f so floor won't produce unexpected results on edges
-            vertex.uv0 = (temp.uv[i] * .9999f + Vector2.one * .000005f);            
-            //encode color index into uv 
-            vertex.uv0 += Vector2.right * colorIndex;
-
+            vertex.uv0 = ((Vector2)temp.uv[i] * .9999f + Vector2.one * .000005f) + Vector2.right * colorIndex;
             toFill.SetUIVertex(vertex, i);
         }
     }
